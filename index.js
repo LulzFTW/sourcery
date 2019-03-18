7d66be7d508390123f95eb17061bd180470ec3e8
LulzFTW <LulzFTW@users.noreply.github.com>
2019-03-17 21:53:54 -0400

    Add countering functionality

--- index.js
+++ index.js
@@ -1,3 +1,55 @@
-let user = require('steam-user');
-let spells = require('./spells');
+const SteamUser = require('steam-user');
+const spells = require('./spells');
 
+require('dotenv').config();
+
+let client = new SteamUser();
+client.logOn({
+    accountName: process.env.username,
+    password: process.env.password
+});
+
+client.on('loggedOn', () => {
+    console.log(`Logged on as '${process.env.username}'`);
+});
+
+client.chat.on('friendMessage', msg => {
+    let text = msg.message;
+    let counter = text.split(/\n+/).map(onMessage)
+        .filter(Boolean).join('\n');
+    if (counter) {
+        client.chat.sendFriendMessage(msg.steamid_friend, counter);
+    }
+});
+
+let buf = [];
+function onMessage(message) {
+    let counter = null;
+
+    if (message.length == 1) {
+        if (message == "!") {
+            counter = counterSpell(buf.join(''), true);
+            buf = [];
+        } else {
+            buf.push(message.charAt(0));
+        }
+    } else {
+        let match = message.match(/\w+!/g) || [];
+        counter = match.map(m => counterSpell(m.slice(0, -1), false))
+            .filter(Boolean).join(' ');
+        buf = [];
+    }
+
+    return counter;
+}
+
+function counterSpell(spell, newline) {
+    let counter = spells.counter(spell);
+    if (counter) {
+        let msg = counter + '!';
+        if (newline)
+            msg = msg.split('').join('\n');
+        return msg;
+    }
+    return null;
+}
